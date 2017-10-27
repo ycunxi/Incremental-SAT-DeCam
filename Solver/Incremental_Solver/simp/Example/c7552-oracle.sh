bf933ef3163758db2ad5fc4af18b13bd72561994
ycunxi <cunxi@broccoli.ecs.umass.edu>
2017-10-27 10:19:33 -0400

    solver upload

---
+++ Solver/Incremental_Solver/simp/Example/c7552-oracle.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "hello world"
+python Oracle.py c7552-abcmap-fmt.v PI.txt PO.txt
---
+++ Solver/Incremental_Solver/simp/depend.mk
@@ -0,0 +1,37 @@
+/home/cunxi/Documents/Incremental-SAT-DeCam/Solver/Incremental_Solver/simp/Main.o /home/cunxi/Documents/Incremental-SAT-DeCam/Solver/Incremental_Solver/simp/Main.or /home/cunxi/Documents/Incremental-SAT-DeCam/Solver/Incremental_Solver/simp/Main.od /home/cunxi/Documents/Incremental-SAT-DeCam/Solver/Incremental_Solver/simp/Main.op: \
+ /home/cunxi/Documents/Incremental-SAT-DeCam/Solver/Incremental_Solver/simp/Main.cc \
+ /home/cunxi/Documents/Incremental-SAT-DeCam/Solver/Incremental_Solver/utils/System.h \
+ /home/cunxi/Documents/Incremental-SAT-DeCam/Solver/Incremental_Solver/mtl/IntTypes.h \
+ /home/cunxi/Documents/Incremental-SAT-DeCam/Solver/Incremental_Solver/utils/ParseUtils.h \
+ /home/cunxi/Documents/Incremental-SAT-DeCam/Solver/Incremental_Solver/utils/Options.h \
+ /home/cunxi/Documents/Incremental-SAT-DeCam/Solver/Incremental_Solver/mtl/Vec.h \
+ /home/cunxi/Documents/Incremental-SAT-DeCam/Solver/Incremental_Solver/mtl/XAlloc.h \
+ /home/cunxi/Documents/Incremental-SAT-DeCam/Solver/Incremental_Solver/incre/incre.h \
+ /home/cunxi/Documents/Incremental-SAT-DeCam/Solver/Incremental_Solver/simp/SimpSolver.h \
+ /home/cunxi/Documents/Incremental-SAT-DeCam/Solver/Incremental_Solver/mtl/Queue.h \
+ /home/cunxi/Documents/Incremental-SAT-DeCam/Solver/Incremental_Solver/core/Solver.h \
+ /home/cunxi/Documents/Incremental-SAT-DeCam/Solver/Incremental_Solver/mtl/Heap.h \
+ /home/cunxi/Documents/Incremental-SAT-DeCam/Solver/Incremental_Solver/mtl/Alg.h \
+ /home/cunxi/Documents/Incremental-SAT-DeCam/Solver/Incremental_Solver/core/SolverTypes.h \
+ /home/cunxi/Documents/Incremental-SAT-DeCam/Solver/Incremental_Solver/mtl/Map.h \
+ /home/cunxi/Documents/Incremental-SAT-DeCam/Solver/Incremental_Solver/mtl/Alloc.h \
+ /home/cunxi/Documents/Incremental-SAT-DeCam/Solver/Incremental_Solver/core/Dimacs.h \
+ /home/cunxi/Documents/Incremental-SAT-DeCam/Solver/Incremental_Solver/incre/cmdline.h \
+ /home/cunxi/Documents/Incremental-SAT-DeCam/Solver/Incremental_Solver/incre/progress.h
+/home/cunxi/Documents/Incremental-SAT-DeCam/Solver/Incremental_Solver/simp/SimpSolver.o /home/cunxi/Documents/Incremental-SAT-DeCam/Solver/Incremental_Solver/simp/SimpSolver.or /home/cunxi/Documents/Incremental-SAT-DeCam/Solver/Incremental_Solver/simp/SimpSolver.od /home/cunxi/Documents/Incremental-SAT-DeCam/Solver/Incremental_Solver/simp/SimpSolver.op: \
+ /home/cunxi/Documents/Incremental-SAT-DeCam/Solver/Incremental_Solver/simp/SimpSolver.cc \
+ /home/cunxi/Documents/Incremental-SAT-DeCam/Solver/Incremental_Solver/mtl/Sort.h \
+ /home/cunxi/Documents/Incremental-SAT-DeCam/Solver/Incremental_Solver/mtl/Vec.h \
+ /home/cunxi/Documents/Incremental-SAT-DeCam/Solver/Incremental_Solver/mtl/IntTypes.h \
+ /home/cunxi/Documents/Incremental-SAT-DeCam/Solver/Incremental_Solver/mtl/XAlloc.h \
+ /home/cunxi/Documents/Incremental-SAT-DeCam/Solver/Incremental_Solver/simp/SimpSolver.h \
+ /home/cunxi/Documents/Incremental-SAT-DeCam/Solver/Incremental_Solver/mtl/Queue.h \
+ /home/cunxi/Documents/Incremental-SAT-DeCam/Solver/Incremental_Solver/core/Solver.h \
+ /home/cunxi/Documents/Incremental-SAT-DeCam/Solver/Incremental_Solver/mtl/Heap.h \
+ /home/cunxi/Documents/Incremental-SAT-DeCam/Solver/Incremental_Solver/mtl/Alg.h \
+ /home/cunxi/Documents/Incremental-SAT-DeCam/Solver/Incremental_Solver/utils/Options.h \
+ /home/cunxi/Documents/Incremental-SAT-DeCam/Solver/Incremental_Solver/utils/ParseUtils.h \
+ /home/cunxi/Documents/Incremental-SAT-DeCam/Solver/Incremental_Solver/core/SolverTypes.h \
+ /home/cunxi/Documents/Incremental-SAT-DeCam/Solver/Incremental_Solver/mtl/Map.h \
+ /home/cunxi/Documents/Incremental-SAT-DeCam/Solver/Incremental_Solver/mtl/Alloc.h \
+ /home/cunxi/Documents/Incremental-SAT-DeCam/Solver/Incremental_Solver/utils/System.h
